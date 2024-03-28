eaf0d0c48be2ac0c96ae4b063145d6e2fe61cce7
campishome <campnapat007@gmail.com>
2024-03-28 14:18:33 +0700

    file upload_5

--- app.ts
+++ app.ts
@@ -5,7 +5,7 @@ import { router as user } from "./API/user";
 import { router as main } from "./API/main";
 import { router as image } from "./API/image";
 import { router as time } from "./API/time";
-import { router as image_upload } from "./API/file_upload";
+// import { router as image_upload } from "./API/file_upload";
 import bodyParser from "body-parser";
 
 export const app = express();
@@ -21,6 +21,6 @@ app.use("/user", user);
 app.use("/main", main);
 app.use("/image", image);
 app.use("/time", time);
-app.use("/file", image_upload);
+// app.use("/file", image_upload);
 
 module.exports = app;
